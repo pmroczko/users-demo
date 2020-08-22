3a4d92a7fd551980974b5543fafd948fdf7ad93b
pawel.mroczkowski <mroczkowsky@gmail.com>
2020-08-22 19:52:43 +0200

    Added filtering.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,29 +1,39 @@
-import React, { useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Users = () => {
-  //const [searchText, setSearchText] = useState("");
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  useEffect(() => {
+    const demoData = [
+      {
+        id: 1,
+        name: "John Doe",
+        tag: "John",
+      },
+      {
+        id: 2,
+        name: "Henry Ford",
+        tag: "Henry",
+      },
+      {
+        id: 3,
+        name: "Jane Smith",
+        tag: "Jane",
+      },
+    ];
+    setUsers(demoData);
+    setFilteredUsers(demoData);
+  }, []);
   const searchText = useRef("");
-  const users = [
-    {
-      id: 1,
-      name: "John Doe",
-      tag: "John",
-    },
-    {
-      id: 2,
-      name: "Henry Ford",
-      tag: "Henry",
-    },
-    {
-      id: 3,
-      name: "Jane Smith",
-      tag: "Jane",
-    },
-  ];
 
   const onChange = (e) => {
-    const value = e.target.value;
-    console.log(`onChange with new Value ${value}`);
+    const filterText = e.target.value;
+    const filteredUsers = users.filter((u) => {
+      const isName = u.name !== undefined && u.name.indexOf(filterText) > -1;
+      const isTag = u.tag !== undefined && u.tag.indexOf(filterText) > -1;
+      return isName || isTag;
+    });
+    setFilteredUsers(filteredUsers);
   };
 
   return (
@@ -38,13 +48,14 @@ const Users = () => {
               placeholder='Search Users...'
               ref={searchText}
               onChange={onChange}
+              autoComplete='off'
             />
           </div>
         </form>
       </div>
       <div className='user-container'>
         <ul>
-          {users.map((u) => (
+          {filteredUsers.map((u) => (
             <li className='left-align' key={u.id}>
               <span className='grayed-out'>{u.id}</span>
               <span className='user-name'> {u.name}</span>
